c4590004141156a55eb8a5fe1f96b938247ff9e3
star-cheng <1335929010@qq.com>
2025-02-21 14:50:52 +0800

    新增setup.sh

---
+++ setup.sh
@@ -0,0 +1,6 @@
+apt update
+apt upgrade -y
+apt install git -y
+git clone https://github.com/Star-Cheng/kajima.git
+git clone https://github.com/Star-Cheng/xv_sdk.git
+git clone https://github.com/Star-Cheng/xvsdk.git
